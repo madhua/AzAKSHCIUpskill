31a4e7ea815c0aa14babd6224ce3eaf3addf94a6
Madhu Sudhan Reddy Aravabhumi <madhu68.amsr@gmail.com>
2023-01-12 19:30:05 +0530

    Update firstfile.ps1

--- firstfile.ps1
+++ firstfile.ps1
@@ -7,7 +7,7 @@ Write-Host "Running Powershell core version $((Get-Host).Version) on $([Environm
 Write-Host "Reading data from json file" $jsonFile
 Write-Host "=====================JSON File data===================="
 $JSON = (Get-Content "$jsonFile" | Out-String | ConvertFrom-Json)
-Write-Host "Cluster Name" $(JSON.clustername)"
+Write-Host "Cluster Name $(JSON.clustername)"
 #foreach($node in $($JSON.cluster_nodes)) 
 #{
 #    Write-Host "Hostname: $($node.name), IP: $($node.ip)"
