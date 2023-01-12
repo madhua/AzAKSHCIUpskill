70a4d9654f503c9a610c659d15bc4d8e0490585a
Madhu Sudhan Reddy Aravabhumi <madhu68.amsr@gmail.com>
2023-01-12 19:07:48 +0530

    Update firstfile.ps1

--- firstfile.ps1
+++ firstfile.ps1
@@ -1,3 +1,14 @@
+#! /usr/bin/env pwsh
+
+param([string]$jsonFile)
+
 Write-Host "Running step-1 script"
 Write-Host "Running Powershell core version $((Get-Host).Version) on $([Environment]::MachineName)..."
+Write-Host "Reading data from json file" $jsonFile
+Write-Host "=====================JSON File data===================="
+$JSON = Get-Content "$jsonFile" | ConvertFrom-Json
+$JSON | Select-Object -Property hostname,ip | ForEach-Object {
+    Write-Host "Hostname:$_.hostname, IP: $_.ip"
+}
+Write-Host "======================================================="
 Write-Host "step-1 Completed"
