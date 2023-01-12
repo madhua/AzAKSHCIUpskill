65257073838aedc61f27a022706356052385d703
Madhu Sudhan Reddy Aravabhumi <madhu68.amsr@gmail.com>
2023-01-12 16:44:09 +0530

    Update firstfile.ps1

--- firstfile.ps1
+++ firstfile.ps1
@@ -1,3 +1,3 @@
 Write-Host "Running step-1 script"
 Write-Host "Running Powershell core version $((Get-Host).Version) on $([Environment]::MachineName)..."
-Wrire-Host "step-1 Completed"
+Write-Host "step-1 Completed"
