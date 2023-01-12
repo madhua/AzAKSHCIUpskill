b708e4c976a2e6f50cf32a360581eec6d21bde03
Madhu Sudhan Reddy Aravabhumi <madhu68.amsr@gmail.com>
2023-01-12 19:13:00 +0530

    Update firstfile.ps1

--- firstfile.ps1
+++ firstfile.ps1
@@ -8,7 +8,7 @@ Write-Host "Reading data from json file" $jsonFile
 Write-Host "=====================JSON File data===================="
 $JSON = Get-Content "$jsonFile" | ConvertFrom-Json
 $JSON | Select-Object -Property hostname,ip | ForEach-Object {
-    Write-Host "Hostname:$_.hostname, IP: $_.ip"
+    Write-Host "Hostname:" $_.hostname, "IP:" $_.ip
 }
 Write-Host "======================================================="
 Write-Host "step-1 Completed"
