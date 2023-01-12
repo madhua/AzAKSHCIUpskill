6322a2e6a753a504e3497f72b41f70c5c32c964e
Madhu Sudhan Reddy Aravabhumi <madhu68.amsr@gmail.com>
2023-01-12 19:28:02 +0530

    Update firstfile.ps1

--- firstfile.ps1
+++ firstfile.ps1
@@ -8,9 +8,9 @@ Write-Host "Reading data from json file" $jsonFile
 Write-Host "=====================JSON File data===================="
 $JSON = (Get-Content "$jsonFile" | Out-String | ConvertFrom-Json)
 Write-Host "Cluster Name" $(JSON.clustername)"
-foreach($node in $($JSON.cluster_nodes)) 
-{
-    Write-Host "Hostname: $($node.name), IP: $($node.ip)"
-}
+#foreach($node in $($JSON.cluster_nodes)) 
+#{
+#    Write-Host "Hostname: $($node.name), IP: $($node.ip)"
+#}
 Write-Host "======================================================="
 Write-Host "step-1 Completed"
