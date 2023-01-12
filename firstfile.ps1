082dddeb7d72489968c1a0f4ebeb493467294a68
Madhu Sudhan Reddy Aravabhumi <madhu68.amsr@gmail.com>
2023-01-12 19:22:42 +0530

    Update firstfile.ps1

--- firstfile.ps1
+++ firstfile.ps1
@@ -6,9 +6,10 @@ Write-Host "Running step-1 script"
 Write-Host "Running Powershell core version $((Get-Host).Version) on $([Environment]::MachineName)..."
 Write-Host "Reading data from json file" $jsonFile
 Write-Host "=====================JSON File data===================="
-$JSON = Get-Content "$jsonFile" | ConvertFrom-Json
-$JSON | Select-Object -Property name,ip | ForEach-Object {
-    Write-Host "Hostname:" $_.name, "IP:" $_.ip
+$JSON = (Get-Content "$jsonFile" | Out-String | ConvertFrom-Json)
+Write-Host "Cluster Name" $(JSON.clustername)"
+foreach($node in ${$JSON.cluster_nodes}) {
+    Write-Host "Hostname: $($node.name), IP: $($node.ip)"
 }
 Write-Host "======================================================="
 Write-Host "step-1 Completed"
